014d0dc5cc4ae62bee1d5d8dbbd2867457e937c9
clearisoft <clearisoft@aliyun.com>
2023-05-11 15:58:51 +0800

    not point ssl_version

--- https.py
+++ https.py
@@ -18,6 +18,5 @@ httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHand
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
                                certfile='./cert.pem',
-                               keyfile="./key.pem",
-                               ssl_version=ssl.PROTOCOL_TLSv1)
+                               keyfile="./key.pem")
 httpd.serve_forever()
